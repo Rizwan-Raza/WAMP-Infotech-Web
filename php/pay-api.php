c8776caa66598c1f587f87bd90000a1ea1d3574c
Rizwan Raza <rizwan.raza987@gmail.com>
2019-02-02 14:32:45 +0530

    Payment Gateway Added

---
+++ php/pay-api.php
@@ -0,0 +1,31 @@
+<?php
+    require("src/Instamojo.php");
+    require 'db.php';
+    $conn = DB::getConnection();
+        
+   $purpose = $_POST['purpose'];
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $phone = $_POST['phone'];
+   $amount = $_POST['amount'];
+   $api = new Instamojo\Instamojo("130191b5fe64c71741350c7758d8cb20", "cf815c6b1d8ad8fa9598c5fd5561155b");
+
+    try {
+        $response = $api->paymentRequestCreate(array(
+            "purpose" => $purpose,
+            "buyer_name"=>$name,
+            "amount" => $amount,
+            "phone"=>$phone,
+            "send_email" => true,
+            "send_sms"=>true,
+            "email" => $email,
+            "redirect_url" => "https://www.wampinfotech.com/pay"
+        ));
+        $id = $conn->real_escape_string($response['id']);
+        # print_r($response);
+        $sql = "INSERT INTO payments(`name`,`email`,`phone`,`amount`,`insta_payment_id`) VALUES('$name','$email','$phone','$amount','$id')";
+        $conn->query($sql);
+        header("Location:$response[longurl]");
+    } catch (Exception $e) {
+        print('Error: ' . $e->getMessage());
+    }
